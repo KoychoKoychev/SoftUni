4ff2a72d1ccb761233a5fe9d6b6861b4408fa8ee
KoychoKoychev <90497061+KoychoKoychev@users.noreply.github.com>
2021-09-15 20:57:41 +0300

    Delete 1. Syntax, Functions and Statements directory

--- 1. Syntax, Functions and Statements/10.aggregateElements.js
+++
@@ -1,5 +0,0 @@
-function aggregateElements(arr){
-    console.log(arr.reduce((a, b) => a + b,0));
-    console.log(arr.map(x => 1/x).reduce((a, b) => a + b,0));
-    console.log(arr.join(""));
-}
--- 1. Syntax, Functions and Statements/6. sumOfNumbers.js
+++
@@ -1,7 +0,0 @@
-function sumOfNum(num1,num2){
-    let sum = 0;
-    for (let i = Number(num1); i<=Number(num2); i++){
-        sum +=i;
-    }
-    console.log(sum);
-}
