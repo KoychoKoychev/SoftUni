c6e038187f8a753b85a182d862073c07f8b62ee1
KoychoKoychev <kgkoychev@gmail.com>
2022-04-01 17:46:29 +0300

    feat: Home page progress

--- CoinBase/src/app/pages/home/home.component.ts
+++ CoinBase/src/app/pages/home/home.component.ts
@@ -7,6 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  actionsArr: any = [
+    {
+      currencyFromLong:'BitCoin',
+      currencyFromShort:'BTC',
+      currencyToLong: 'Bank Transfer',
+      currencyToShort: '$',
+      amountFrom: 1.36,
+      amountTo: 306102.23
+    },
+    {
+      currencyFromLong:'BitCoin',
+      currencyFromShort:'BTC',
+      currencyToLong: 'Bank Transfer',
+      currencyToShort: '$',
+      amountFrom: 1.66,
+      amountTo: 406102.23
+    },
+    {
+      currencyFromLong:'Etherium',
+      currencyFromShort:'ETH',
+      currencyToLong: 'Bank Transfer',
+      currencyToShort: '$',
+      amountFrom: 2.16,
+      amountTo: 506102.23
+    },
+    {
+      currencyFromLong:'BitCoin',
+      currencyFromShort:'BTC',
+      currencyToLong: 'Bank Transfer',
+      currencyToShort: '$',
+      amountFrom: 2.76,
+      amountTo: 606102.23
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
