fbc5713ed13e3afbe49dc09f7030e72b7a3673bc
KoychoKoychev <kgkoychev@gmail.com>
2022-04-03 14:23:17 +0300

    feat: Home page styling finalized

--- CoinBase/src/app/comp/comp.module.ts
+++ CoinBase/src/app/comp/comp.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';
+import { BlogCardComponent } from './blog-card/blog-card.component';
 
 @NgModule({
   declarations: [
-    HeroComponent
+    HeroComponent,
+    BlogCardComponent
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    HeroComponent
+    HeroComponent,
+    BlogCardComponent
   ]
 })
 export class CompModule { }
--- CoinBase/src/app/pages/home/home.component.ts
+++ CoinBase/src/app/pages/home/home.component.ts
@@ -9,37 +9,58 @@ export class HomeComponent implements OnInit {
 
   actionsArr: any = [
     {
-      currencyFromLong:'BitCoin',
-      currencyFromShort:'BTC',
+      currencyFromLong: 'BitCoin',
+      currencyFromShort: 'BTC',
       currencyToLong: 'Bank Transfer',
       currencyToShort: '$',
       amountFrom: 1.36,
       amountTo: 306102.23
     },
     {
-      currencyFromLong:'BitCoin',
-      currencyFromShort:'BTC',
+      currencyFromLong: 'BitCoin',
+      currencyFromShort: 'BTC',
       currencyToLong: 'Bank Transfer',
       currencyToShort: '$',
       amountFrom: 1.66,
       amountTo: 406102.23
     },
     {
-      currencyFromLong:'Etherium',
-      currencyFromShort:'ETH',
+      currencyFromLong: 'Etherium',
+      currencyFromShort: 'ETH',
       currencyToLong: 'Bank Transfer',
       currencyToShort: '$',
       amountFrom: 2.16,
       amountTo: 506102.23
     },
     {
-      currencyFromLong:'BitCoin',
-      currencyFromShort:'BTC',
+      currencyFromLong: 'BitCoin',
+      currencyFromShort: 'BTC',
       currencyToLong: 'Bank Transfer',
       currencyToShort: '$',
       amountFrom: 2.76,
       amountTo: 606102.23
     }
+  ]
+
+  blogPostsArr: any = [
+    {
+      created_at: new Date().toLocaleDateString('en-GB'),
+      title: 'ETH TO NAIRA EXCHANGE RATE IS NOT #400 PER A SINGLE COIN',
+      category: 'Business',
+      content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged'
+    },
+    {
+      created_at: new  Date().toLocaleDateString('en-GB'),
+      title: 'ETH TO NAIRA EXCHANGE RATE IS NOT #400 PER A SINGLE COIN',
+      category: 'Business',
+      content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged'
+    },
+    {
+      created_at: new Date().toLocaleDateString('en-GB'),
+      title: 'ETH TO NAIRA EXCHANGE RATE IS NOT #400 PER A SINGLE COIN',
+      category: 'Business',
+      content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged'
+    },
   ]
 
   constructor() { }
